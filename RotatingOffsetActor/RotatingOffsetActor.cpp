dae7d22aae666df56e09244cd4c849a607d22907
Harrison <hmcguire13@gmail.com>
2017-11-03 09:11:35 -0400

    errors

--- RotatingOffsetActor/RotatingOffsetActor.cpp
+++ RotatingOffsetActor/RotatingOffsetActor.cpp
@@ -24,13 +24,28 @@ void ARotatingOffsetActor::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 	// rotate and offset actor
-	FRotator NewRotation = FRotator(PitchValue, YawValue, RollValue);
-	FVector NewLocation = FVector(XValue, YValue, ZValue);
+	// FRotator NewRotation = FRotator(PitchValue, YawValue, RollValue);
+	// NewLocation.X += 1;
+	// FVector NewLocation = FVector(XValue, YValue, ZValue);
+
+
+	// FVector NewLocation = RotateVectorAroundAxis(GetActorLocation(), 50.0, (0,0,0));
+	// SetActorLocation(NewLocation);
+
+	// FVector NewLocation = GetActorLocation();	
+	// FVector NewLoc = NewLocation.RotateAngleAxis(30,FVector(0,0,1));
+	// GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Some variable values: x: %s"), *NewLoc().ToString()));
 	
-	FQuat QuatRotation = FQuat(NewRotation);
 	
-	AddActorLocalOffset(NewLocation, false, 0, ETeleportType::None);
-	AddActorLocalRotation(QuatRotation, false, 0, ETeleportType::None);
+	// FQuat QuatRotation = FQuat(NewRotation);
+	
+	// AddActorLocalOffset(NewLocation, false, 0, ETeleportType::None);
+	// AddActorLocalRotation(QuatRotation, false, 0, ETeleportType::None);
+
+	const FVector ActorPosition = GetActorLocation();
+	const FVector NewActorLocation = ActorPosition.RotateAngleAxis(45.f, GetActorUpVector());
+	
+	SetActorLocation(NewActorLocation);
 	
 }
 
