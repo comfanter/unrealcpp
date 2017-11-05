6b92bba7c03ac8717327f998f3695029fc1da2ff
Harrison <hmcguire13@gmail.com>
2017-11-05 07:42:08 -0500

    find player location class

---
+++ FindPlayerPosition/FindPlayerPosition.cpp
@@ -0,0 +1,33 @@
+// Harrison McGuire UE4 Version 4.18 https://github.com/Harrison1/unrealcpp https://severallevels.io
+
+#include "FindPlayerPosition.h"
+
+
+// Sets default values
+AFindPlayerPosition::AFindPlayerPosition()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFindPlayerPosition::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFindPlayerPosition::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// get first player pawn location
+	FVector myCharacter = GetWorld()->GetFirstPlayerController()->GetPawn()->GetActorLocation();
+
+	// screen log player location
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, FString::Printf(TEXT("Player Location: %s"), *myCharacter.ToString()));	
+
+}
+
---
+++ FindPlayerPosition/FindPlayerPosition.h
@@ -0,0 +1,28 @@
+// Harrison McGuire UE4 Version 4.18 https://github.com/Harrison1/unrealcpp https://severallevels.io
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FindPlayerPosition.generated.h"
+
+UCLASS()
+class UNREALCPP_API AFindPlayerPosition : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFindPlayerPosition();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
