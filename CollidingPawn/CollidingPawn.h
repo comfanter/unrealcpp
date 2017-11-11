7b3cd37d22a6061dd6e4dfc244d516ea83614b77
Harrison <hmcguire13@gmail.com>
2017-11-11 16:02:04 -0500

    added collision pawn

---
+++ CollidingPawn/CollidingPawn.h
@@ -0,0 +1,40 @@
+// Harrison McGuire UE4 Version 4.18 https://github.com/Harrison1/unrealcpp https://severallevels.io
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "CollidingPawn.generated.h"
+
+UCLASS()
+class UNREALCPP_API ACollidingPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ACollidingPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+
+	// Called every frame
+    virtual void Tick( float DeltaSeconds ) override;
+
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+
+    UParticleSystemComponent* OurParticleSystem;
+    class UCollidingPawnMovementComponent* OurMovementComponent;
+
+    virtual UPawnMovementComponent* GetMovementComponent() const override;
+
+    void MoveForward(float AxisValue);
+    void MoveRight(float AxisValue);
+    void Turn(float AxisValue);
+    void ParticleToggle();
+
+};
