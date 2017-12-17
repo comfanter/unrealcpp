0fcc723f74dbc629a9e176d24696c86378f5f8e0
Harrison <hmcguire13@gmail.com>
2017-12-16 20:44:26 -0500

    typo edits

--- FindPlayerPosition/FindPlayerPosition.cpp
+++ FindPlayerPosition/FindPlayerPosition.cpp
@@ -1,4 +1,7 @@
-// Harrison McGuire UE4 Version 4.18 https://github.com/Harrison1/unrealcpp https://severallevels.io
+// Harrison McGuire 
+// UE4 Version 4.18.2 
+// https://github.com/Harrison1/unrealcpp 
+// https://severallevels.io
 
 #include "FindPlayerPosition.h"
 
