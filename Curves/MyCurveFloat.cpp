3c1183d99ec9b17297a05baffd266f7b02384b9e
Harrison <hmcguire13@gmail.com>
2018-01-31 07:39:02 -0500

    start curve

---
+++ Curves/MyCurveFloat.cpp
@@ -0,0 +1,11 @@
+// Harrison McGuire
+// UE4 Version 4.18.3
+// https://github.com/Harrison1/unrealcpp
+// https://severallevels.io
+// https://harrisonmcguire.com
+
+#include "MyCurveFloat.h"
+
+
+
+
