41e29e251c62b924685d45b830972c54312aa2d5
Harrison <hmcguire13@gmail.com>
2018-02-01 07:02:33 -0500

    deleted curve folder and added opendoortimelinecurve actoer wip

---
+++ OpenDoorTimelineCurve/OpenDoorTimelineCurve.cpp
@@ -0,0 +1,31 @@
+// Harrison McGuire
+// UE4 Version 4.18.3
+// https://github.com/Harrison1/unrealcpp
+// https://severallevels.io
+// https://harrisonmcguire.com
+
+#include "OpenDoorTimelineCurve.h"
+
+
+// Sets default values
+AOpenDoorTimelineCurve::AOpenDoorTimelineCurve()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AOpenDoorTimelineCurve::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AOpenDoorTimelineCurve::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ OpenDoorTimelineCurve/OpenDoorTimelineCurve.h
@@ -0,0 +1,32 @@
+// Harrison McGuire
+// UE4 Version 4.18.3
+// https://github.com/Harrison1/unrealcpp
+// https://severallevels.io
+// https://harrisonmcguire.com
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "OpenDoorTimelineCurve.generated.h"
+
+UCLASS()
+class UNREALCPP_API AOpenDoorTimelineCurve : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AOpenDoorTimelineCurve();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
